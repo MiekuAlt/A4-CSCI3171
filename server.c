d3a6b0fc25ac542af5159a21c415a5f554c80ae0
Michael <m.altair@live.com>
2017-07-11 00:51:19 -0300

    Playing around with Atom, don't mind me!

--- server.c
+++ server.c
@@ -1,11 +1,13 @@
 /* A simple server in the internet domain using TCP
-   The port number is passed as an argument 
-   This version runs forever, forking off a separate 
+   The port number is passed as an argument
+   This version runs forever, forking off a separate
    process for each connection
    gcc server2.c -lsocket
 */
+
+// Just testing something!
 #include <stdio.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
@@ -26,7 +28,7 @@ int main(int argc, char *argv[])
          exit(1);
      }
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
+     if (sockfd < 0)
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
      portno = atoi(argv[1]);
@@ -34,14 +36,14 @@ int main(int argc, char *argv[])
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
+              sizeof(serv_addr)) < 0)
               error("ERROR on binding");
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
      while (1) {
-         newsockfd = accept(sockfd, 
+         newsockfd = accept(sockfd,
                (struct sockaddr *) &cli_addr, &clilen);
-         if (newsockfd < 0) 
+         if (newsockfd < 0)
              error("ERROR on accept");
          pid = fork();
          if (pid < 0)
@@ -57,7 +59,7 @@ int main(int argc, char *argv[])
 }
 
 /******** DOSTUFF() *********************
- There is a separate instance of this function 
+ There is a separate instance of this function
  for each connection.  It handles all communication
  once a connnection has been established.
  *****************************************/
@@ -65,7 +67,7 @@ void dostuff (int sock)
 {
    int n;
    char buffer[256];
-      
+
    bzero(buffer,256);
    n = read(sock,buffer,255);
    if (n < 0) error("ERROR reading from socket");
